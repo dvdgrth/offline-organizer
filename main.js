5b083a81039fcc37e1a848973623ea8600c643e5
David Guerth <david.guerth@gmail.com>
2019-04-22 11:36:12 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("add").onclick = () => {
+        var input = document.getElementById("new-todo").value;
+        if (input == "") {
+            alert("Bitte etwas eingeben!")
+        }
+        else {
+            add_element(input, undefined);
+        }
+    }
+
+    if (!("i" in localStorage)) {
+        localStorage.setItem("i", 0);
+    }
+
+
+for (const key in localStorage) {
+    if (parseInt(key)) {
+        add_element(localStorage[key], key)
+    }
+}
+
+});
+
+function add_element(input, register) {
+    const new_el = document.createElement('li');
+    if (!register) {
+        i = parseInt(localStorage["i"]);
+        new_el.dataset.id = i;
+        localStorage.setItem(i, input);
+        i++;
+        localStorage["i"] = i;
+    }
+    else {
+        new_el.dataset.id = register;
+    }
+    new_el.innerHTML = input;
+    document.getElementById("todo-list").appendChild(new_el);
+    const new_span = document.createElement('span');
+    new_el.appendChild(new_span);
+    new_span.className = "delete";
+    new_span.innerHTML = "x";
+    new_span.onclick = () => {
+        new_span.parentNode.style.display = "none";
+        localStorage.removeItem(new_span.parentNode.dataset.id);
+    }
+}
